1ce1bc288ae668c980102262dfca99bbe9e88a49
mahmoud <nadymahmoud016@gmail.com>
2026-07-22 00:02:01 +0300

    Add bonus.js

---
+++ bonus.js
@@ -0,0 +1,21 @@
+//bouns
+var createCounter = function (init) {
+    let current = init;
+
+    return {
+        increment: function () {
+            current++;
+            return current;
+        },
+
+        decrement: function () {
+            current--;
+            return current;
+        },
+
+        reset: function () {
+            current = init;
+            return current;
+        }
+    };
+};
